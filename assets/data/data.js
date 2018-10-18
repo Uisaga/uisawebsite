8672cd151e0b7b29bf1104fffb288671ed9a0d9e
Uisaga <uisageorgia@gmail.com>
2018-10-18 00:01:57 -0400

    Update data.js

--- assets/data/data.js
+++ assets/data/data.js
@@ -10,7 +10,7 @@ var teams = [
     {"pic":"assets/data/teams/QCI.png", "teamName":"Queen City Ishaare", "category":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/UISAOfficial/?ref=aymt_homepage_panel"},
     {"pic":"assets/data/teams/BBB.png", "teamName":"Bham's Blazin Bhangra", "category":"Bhangra", "facebookLink": "https://www.facebook.com/UISAOfficial/?ref=aymt_homepage_panel"},
 
-    {"pic":"assets/data/teams/CCB.png", "teamName":"Classic City Bhangra", "category":"Bhangra", "facebookLink": "https://www.facebook.com/UISAOfficial/?ref=aymt_homepage_panel"},
+    {"pic":"assets/data/teams/CCB.PNG", "teamName":"Classic City Bhangra", "category":"Bhangra", "facebookLink": "https://www.facebook.com/UISAOfficial/?ref=aymt_homepage_panel"},
     {"pic":"assets/data/teams/58889568bc2fc2ef3a1860bc.png", "teamName":"Coming Soon", "category":"Bhangra", "facebookLink": "https://www.facebook.com/UISAOfficial/?ref=aymt_homepage_panel"},
    
     
